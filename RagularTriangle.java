932a12fd52295260361ded1075fd473f8c1ad562
solitude6060 <solitude6060@gmail.com>
2015-05-17 22:44:40 +0800

    Create RagularTriangle.java

---
+++ RagularTriangle.java
@@ -0,0 +1,52 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class RagularTriangle extends JPanel {
+	private JTextField length = new JTextField(15);
+	private JTextField area = new JTextField(15);
+	private JTextField perimeter = new JTextField(15);
+	
+	double lengthNum,areaNum,perimeterNum;
+	
+	public RagularTriangle(){
+		JButton setlength = new JButton("Calculate");
+		JButton clean = new JButton("Clean");
+		
+		JLabel Llength = new JLabel("Length: ");
+		JLabel Larea = new JLabel("Area: ");
+		JLabel Lperimeter = new JLabel("perimeter: ");
+		
+		setLayout(new GridLayout(2,4));
+		
+		add(Llength);
+		add(length);
+		add(Larea);
+		add(area);
+		add(Lperimeter);
+		add(perimeter);
+		add(setlength);
+		setlength.addActionListener(new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent e){
+				lengthNum = Double.parseDouble(length.getText());
+				areaNum = lengthNum*lengthNum*Math.pow(3, 0.5)*0.25;
+				perimeterNum = lengthNum*3;
+				area.setText(String.valueOf(areaNum));
+				perimeter.setText(String.valueOf(perimeterNum));
+				
+			}
+		});
+		add(clean);
+		clean.addActionListener(new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent e){
+				length.setText(null);
+				area.setText(null);
+				perimeter.setText(null);
+			}
+		});
+		
+		
+	}	
+}
